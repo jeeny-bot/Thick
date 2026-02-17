de328c5c30be33b0fa22d8d0f027ce7de7fb669e
motheryjkim <motheryjkim@gmail.com>
2026-02-17 07:26:12 +0000

    Fix: firebase-config.js 추가하여 빌드 오류 해결

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6Ws2dXPrMqUQTwVbGDdcAhdKJzJK1Cmk",
+    authDomain: "thick-fdd38.firebaseapp.com",
+    projectId: "thick-fdd38",
+    storageBucket: "thick-fdd38.firebasestorage.app",
+    messagingSenderId: "109235677950",
+    appId: "1:109235677950:web:f58eb3bcdfdaea9e3793af",
+    measurementId: "G-LH61G4J09W"
+};
